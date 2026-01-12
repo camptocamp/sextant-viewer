e4a00867671df09ac19cb71a710b4af498401a87
Olivia <olivia.guyot@camptocamp.com>
2026-01-12 10:04:04 +0100

    fix rebase

--- src/components/layout/LayerPanel.vue
+++ src/components/layout/LayerPanel.vue
@@ -1,10 +1,4 @@
 <script setup lang="ts">
-import LayerDetailsPanel from '@/components/layout/LayerDetailsPanel.vue'
-import { useLayersStore } from '@/stores/layers.store.ts'
-import { storeToRefs } from 'pinia'
-
-const { selectedLayer } = storeToRefs(useLayersStore())
-
 const tabItems = [
   { slot: 'list', label: 'List' },
   { slot: 'tree', label: 'Tree' },
@@ -14,13 +8,7 @@ const tabItems = [
 <template>
   <UTabs :items="tabItems" :ui="{ content: 'mt-3 h-full' }" class="sxt-panel">
     <template #list>
-      <div class="flex flex-col gap-2">
-        <LayerManager />
-        <div v-if="selectedLayer">
-          <USeparator icon="i-tabler-stack-2-filled" />
-          <LayerDetailsPanel :layer="selectedLayer" />
-        </div>
-      </div>
+      <LayerManager />
     </template>
 
     <template #tree>
