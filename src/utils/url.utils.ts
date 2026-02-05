73452b7d38609f3c169fb31d8ff5ea8fddd7efab
Romuald Caplier <romuald.caplier@camptocamp.com>
2026-02-05 17:55:11 +0100

    chore: add eslint-plugin-unused-imports, update sdk and odc client, add knipp and remove dead code

--- src/utils/url.utils.ts
+++
@@ -1,9 +0,0 @@
-const URL_REGEX = /\bhttps?:\/\/(?:\([^\s()]+\)|[^\s()]+)+/g
-
-export function extractUrls(value: string): string[] {
-  return [...value.matchAll(URL_REGEX)].map((match) => match[0])
-}
-
-export function isHttpUrl(url: string): boolean {
-  return url.startsWith('http://') || url.startsWith('https://')
-}
