3469ea9df11325519dde7e339b6b34bb667dc1a9
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2026-02-11 17:15:12 +0100

    Remove id and version in demo COG layer

--- demo/index.js
+++ demo/index.js
@@ -102,13 +102,11 @@ ${SDK_GUIDE_LAYERS_DOC}
 viewer.addLayer(
   {
     type: 'geotiff',
-        id: 'cog-oam-brest',
         url: 'https://oin-hotosm-temp.s3.us-east-1.amazonaws.com/66e43a1ecd0baa0001b62135/0/66e43a1ecd0baa0001b62136.tif',
         label: 'COG Brest (OpenAerialMap)',
         visibility: true,
         opacity: 1,
         attributions: '© OpenAerialMap contributors, CC-BY 4.0',
-        version: 0,
       }
     );
 
